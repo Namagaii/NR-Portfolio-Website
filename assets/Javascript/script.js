28d0c4e8a6d8e19a6c230aa755e9f39a3f9ec391
namagaii <rnoel2125@gmail.com>
2022-01-27 04:42:03 -0500

    Prettied up the site with hover effects and added proper data.

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,24 +1,18 @@
 var homeButton = document.getElementById("home-button");
 var webdevButton = document.getElementById("webdev-button");
-var unityButton = document.getElementById("unity-button");
 var homeArea = document.getElementById("home-area");
 var webdevArea = document.getElementById("webdev-area");
-var unityArea = document.getElementById("unity-area");
+var homeBioButton = document.getElementById("bio");
+var homeWebdevButton = document.getElementById("webdev-section");
 var displayType = "flex";
 
 function hideElements(id){
     switch(id){
         case "home-button":
             webdevArea.style.display = "none";
-            unityArea.style.display = "none";
             break;
         case "webdev-button":
             homeArea.style.display = "none";
-            unityArea.style.display = "none";
-            break;
-        case "unity-button":
-            homeArea.style.display = "none";
-            webdevArea.style.display = "none";
             break;
         default: 
             console.log("Unknown Button Pressed");
@@ -30,4 +24,5 @@ homeArea.style.display = displayType;
 hideElements("home-button");
 homeButton.addEventListener("click", function(event){ homeArea.style.display = displayType;  hideElements("home-button");});
 webdevButton.addEventListener("click", function(event){ webdevArea.style.display = displayType; hideElements("webdev-button");});
-unityButton.addEventListener("click", function(event){ unityArea.style.display = displayType; hideElements("unity-button");});
+homeBioButton.addEventListener("click", function(event){ homeArea.style.display = displayType;  hideElements("home-button");});
+homeWebdevButton.addEventListener("click", function(event){ webdevArea.style.display = displayType; hideElements("webdev-button");});
